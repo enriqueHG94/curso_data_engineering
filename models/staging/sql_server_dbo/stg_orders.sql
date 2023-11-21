bee5ef6d608804ef7722e288e22b79e8bdea702a
Enrique Hervás Guerrero <enriquehervasguerrero@gmail.com>
2023-11-21 15:05:28 +0000

    Update

--- models/staging/sql_server_dbo/stg_orders.sql
+++ models/staging/sql_server_dbo/stg_orders.sql
@@ -1,4 +1,4 @@
-with 
+with
 
 source as (
 
@@ -9,23 +9,23 @@ source as (
 renamed_orders as (
 
     select
-        order_id,
-        user_id,
-        address_id,
-        promo_id,
-        tracking_id,
-        created_at,
-        estimated_delivery_at,
-        status,
-        delivered_at,
-        shipping_service,
-        shipping_cost,
-        order_cost,
-        order_total,
-        _fivetran_deleted AS date_deleted,
-        _fivetran_synced AS date_load
+        {{ dbt_utils.generate_surrogate_key(['order_id']) }} as order_id,
+        CAST(USER_ID AS VARCHAR(75)) as user_id,
+        CAST(ADDRESS_ID AS VARCHAR(75)) as address_id,
+        decode(promo_id, null, 'Sin promo', '', 'Sin promo', promo_id) as promo_id,
+        decode(tracking_id, null, '999', '', '999', tracking_id) as tracking_id,
+        created_at as created_at_utc,
+        CAST(status AS VARCHAR(25)),
+        COALESCE(estimated_delivery_at, '1900-01-01 00:00:00.000-0800') as estimated_delivery_at_utc,
+        COALESCE(delivered_at, '1900-01-01 00:00:00.000-0800') as delivered_at_utc,
+        decode(shipping_service, null, '999', '', '999', shipping_service) as shipping_service,
+        CAST(SHIPPING_COST AS DECIMAL(10,2)) as shipping_cost_usd,
+        CAST(ORDER_COST AS DECIMAL(10,2)) as order_cost_usd,
+        CAST(ORDER_TOTAL AS DECIMAL(10,2)) as order_total_usd,
+        _fivetran_deleted as date_deleted,
+        _fivetran_synced as date_load
 
-    from source
+from source
 
 )
 
--- models/staging/sql_server_dbo/stg_promos.sql
+++ models/staging/sql_server_dbo/stg_promos.sql
@@ -1,4 +1,4 @@
-with 
+with
 
 source as (
 
@@ -9,14 +9,23 @@ source as (
 renamed_promos as (
 
     select
-        promo_id,
-        discount,
-        status,
-        _fivetran_deleted AS date_deleted,
-        _fivetran_synced AS date_load
+        {{ dbt_utils.generate_surrogate_key(['promo_id']) }} as promo_id,
+        cast(promo_id as varchar(50)) as desc_promo,
+        cast(discount as number(10,0)) as discount,
+        cast(status as varchar(50)) as status,
+        _fivetran_deleted as date_deleted,
+        _fivetran_synced as date_load
 
-    from source
+from source
 
 )
 
 select * from renamed_promos
+union all
+select
+'999',
+'Sin Promo',
+0,
+'inactive',
+null,
+null
