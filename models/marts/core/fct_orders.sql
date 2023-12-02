296a9845521db0073ff2689062c3462a3fc2f7e4
Enrique Hervás Guerrero <enriquehervasguerrero@gmail.com>
2023-12-02 14:32:59 +0000

    Creating a fact table

---
+++ models/marts/core/fct_orders.sql
@@ -0,0 +1,26 @@
+WITH stg_orders AS (
+    SELECT * 
+    FROM {{ ref('stg_orders') }}
+),
+
+
+final_orders AS (
+    SELECT
+        id_order, 
+        id_user,
+        id_address,
+        id_promo,
+        id_tracking,
+        created_at_utc,
+        DATEDIFF(day, created_at_utc, delivered_at_utc) AS days_to_deliver,
+        status,
+        estimated_delivery_at_utc,
+        delivered_at_utc,
+        shipping_service,
+        shipping_cost_usd,
+        item_order_cost_usd,
+        order_total_usd
+    FROM stg_orders
+    )
+
+SELECT * FROM final_orders
