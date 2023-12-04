e818af3164bbd30faa05d81c8704621f9c837292
Enrique Hervás Guerrero <enriquehervasguerrero@gmail.com>
2023-12-04 15:45:10 +0000

    Improving stgs

--- models/staging/sql_server_dbo/stg_addresses.sql
+++ models/staging/sql_server_dbo/stg_addresses.sql
@@ -14,7 +14,7 @@ renamed_addresses as (
         cast(zipcode as integer) as zipcode,
         cast(state as varchar(75)) as state,
         cast(country as varchar(75)) as country,
-        _fivetran_synced AS raw_timestamp_load -- Para monitorear a partir de que día empieza a reproducirse cierta casuística.
+        _fivetran_synced AS raw_timestamp_load 
     from src_addresses
 
 )
