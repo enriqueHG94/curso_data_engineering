2ef4c2a56f22c7ec9b6572c2fb18fe2b4437d56f
Enrique Hervás Guerrero <enriquehervasguerrero@gmail.com>
2023-12-14 12:51:19 +0000

    Fix

--- models/marts/marketing/user_purchases.sql
+++ models/marts/marketing/user_purchases.sql
@@ -33,41 +33,47 @@ dim_addresses as (
 
 ),
 
--- Pedidos y las líneas de detalle agrupados por usuario y diferentes cálculos para cada usuario.
+
 user_orders as (
     select
-        id_user,
-        count(distinct id_order) as total_orders,
-        sum(order_total_usd) as total_spent,
-        sum(shipping_cost_usd) as total_shipping,
-        sum(order_total_usd * p.discount / 100) as total_discount,
-        sum(quantity) as total_products,
-        count(distinct id_product) as total_different_products
-    from fct_orders_items as o
-    inner join dim_promos as p on o.id_promo = p.id_promo
-    group by id_user
+        f.id_user,
+        count(distinct f.id_order) as total_orders,
+        sum(f.total_product_cost_usd) as total_spent,
+        sum(f.shipping_cost_per_product_line) as total_shipping,
+        sum(f.total_product_cost_usd * (coalesce(p.discount, 0) / 100))
+            as total_discount,
+        sum(f.quantity) as total_products,
+        count(distinct f.id_product) as total_different_products
+    from fct_orders_items as f
+    left join dim_promos as p on f.id_promo = p.id_promo
+    group by f.id_user
+),
+
+user_data as (
+    select
+        u.id_user,
+        u.first_name,
+        u.last_name,
+        u.email,
+        u.phone_number,
+        u.created_profile,
+        u.updated_profile,
+        a.address,
+        a.zipcode,
+        a.state,
+        a.country
+    from dim_users as u
+    left join dim_addresses as a on u.id_address = a.id_address
 )
 
--- Tabla final con la información de cada usuario
 select
-    u.id_user,
-    u.first_name,
-    u.last_name,
-    u.email,
-    u.phone_number,
-    u.created_profile,
-    u.updated_profile,
-    a.address,
-    a.zipcode,
-    a.state,
-    a.country,
-    o.total_orders,
-    o.total_spent,
-    o.total_shipping,
-    o.total_discount,
-    o.total_products,
-    o.total_different_products
-from dim_users as u
-inner join user_orders as o on u.id_user = o.id_user
-inner join dim_addresses as a on u.id_address = a.id_address
-order by u.id_user
+    ud.*,
+    uo.total_orders,
+    uo.total_spent,
+    uo.total_shipping,
+    uo.total_discount,
+    uo.total_products,
+    uo.total_different_products
+from user_data as ud
+inner join user_orders as uo on ud.id_user = uo.id_user
+order by ud.id_user
