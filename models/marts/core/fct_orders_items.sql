15e9d5c3b66c8260bb851d8496e7c332ee5d73d4
Enrique Hervás Guerrero <enriquehervasguerrero@gmail.com>
2023-12-14 12:17:00 +0000

    Update

--- models/marts/core/fct_orders_items.sql
+++ models/marts/core/fct_orders_items.sql
@@ -36,15 +36,18 @@ final_fct as (
         id_tracking,
         status,
         p.name as product_name,
-        p.price as product_price,
+        p.price as product_price_usd,
         quantity,
         (p.price * oi.quantity) as total_product_cost_usd,
+        cast((o.shipping_cost_usd * oi.quantity) / oc.total_quantity as decimal(7,2)) as shipping_cost_per_product_line,
         created_at_utc,
         DATEDIFF(day, created_at_utc, delivered_at_utc) AS days_to_deliver,
         estimated_delivery_at_utc,
         delivered_at_utc,
         shipping_service,
-        cast((o.shipping_cost_usd * oi.quantity) / oc.total_quantity as decimal(7,2)) as shipping_cost_per_product_line
+        shipping_cost_usd,
+        item_order_cost_usd as order_cost_usd,
+        order_total_usd
     from {{ ref('stg_order_items') }}  oi
     join {{ ref('stg_orders') }}  o
     on oi.id_order = o.id_order
